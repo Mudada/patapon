092759ad1511c2fc4206d84120ed698fe5193af7
Mudada <mael.nicolas77@gmail.com>
2019-07-05 15:35:03 +0200

    add a big explanation of either in comment + typeclass

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,91 @@ import * as S from "sanctuary"
 
 //. ### Command
 //# data Part = Pata | Pon | Chaka | Don
-interface Pata {type: "Pata"}
-const Pata = ():Pata => ({type: "Pata"})
 
-interface Pon {type: "Pon"}
-const Pon = ():Pon => ({type: "Pon"})
+interface Type {
+    type: string
+}
 
-interface Chaka {type: "Chaka"}
-const Chaka = ():Chaka => ({type: "Chaka"})
+interface Show {
+    show<T>():string
+}
 
-interface Don {type: "Don"}
-const Don = ():Don => ({type: "Don"})
+class Pata implements Type, Show {
+    type = "Pata"
+    show = () => "pata"
+}
+
+class Pon implements Type, Show {
+    type = "Pon"
+    show = () => "pon"
+}
+
+class Chaka implements Type, Show {
+    type = "Chaka"
+    show = () => "chaka"
+}
+
+class Don implements Type, Show {
+    type = "Don"
+    show = () => "don"
+}
 
 type Part = Pata | Pon | Chaka | Don
 
-//# data Command = Command Part Part Part Part
-type Command = [Part, Part, Part, Part]
+type Song = [Part, Part, Part, Part]
 
-const takePata = (pata :Pata):boolean => true
+const PataPata: Song = [
+     new Pata()
+    ,new Pata()
+    ,new Pata()
+    ,new Pon()
+]
 
-const pataPata = ():Command => [Pata(), Pata(), Pata(), Pon()]
+const show = (showable: Show):string =>
+    showable.show()
 
-// console.log(takePata(Pata())) This work
-// console.log(takePata(Pon())) This fail
+const showSong = (song: Song):string[] =>
+    S.map (show) (song)
+
+/*
+  type Left<A> = A
+  type Right<B> = B
+  type Either<A, B> = Left<A> | Right<B>
+
+  Either<A, B> = Left A | Right B
+  Either<String, Int> = Left String | Right Int
+
+  Maybe<T> = Just T | Nothing
+  Maybe<Int> = Just Int | Nothing
+
+  const head = (Array<Int>):Maybe<Int> => {
+     if ([]) => Nothing
+     else ([0]) => Just x
+  }
+
+  const head = (Array<Int>):Either<String, Int> => {
+  if ([]) => Left "Tableau Vide"
+  else ([0]) => Right [0]
+  }
+
+applyEither<A, B> =
+    (f:((A): X), g((B): X), val:Either<A,B>):X {
+        switch(val.type) {
+            case "Left":
+                return f(val);
+            case "Right":
+                return g(val);
+        }
+    }
+f => return -1
+g => return val
+applyEither f g Left<"error"> // $> -1
+applyEither f g Right<12> // $> 12
+
+map (applyEither (f) (g)) [Left<"error">, Right<1>]
+  // [-1, 1]
+  main {
+     head([1,2,3])
+
+  }
+*/
